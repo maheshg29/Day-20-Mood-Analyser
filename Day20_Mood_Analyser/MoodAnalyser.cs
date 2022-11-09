0e5f5312b3966853c7ee1ddba5c4d5a1f17d0810
mahesh <gangurdemahesh9@gmail.com>
2022-11-09 17:53:20 +0530

    [Mahesh] Add UC1 file

---
+++ Day20_Mood_Analyser/MoodAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_Mood_Analyser
+{
+   public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        //Creating method for Analysing the mood
+        public string AnalyseMood()
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
--- Day20_Mood_Analyser/Program.cs
+++ Day20_Mood_Analyser/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Day20_Mood_Analyser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 20 Mood Analyser Problem");
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am Sad Mood");
+            string checkmood = moodAnalyser.AnalyseMood();
+            Console.WriteLine(checkmood);
+        }
+    }
+}
